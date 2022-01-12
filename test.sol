3011d543da6cb8447ecc1177d9f328c8413d39e3
Elfss <elfenho@gmail.com>
2022-01-12 18:33:29 -0300

    epoch

--- test.sol
+++ test.sol
@@ -65,4 +65,41 @@ contract Structs {
     function incrementCount() internal {
         peopleCount += 1;
     }
+
+    /* 
+        Também é possivel usar o epoch-time para definir a que momento um contrato começar a ser usado,-
+        isso pode ser aplicado no lugar do owner nesse exemplo e ja que os contratos são imutaveis é uma boa- 
+        opção para fazer pre-sales ou liberar contratos em datas específicas.
+        outro uso util do epoch-time é a possibilidade de "travar" uma carteira principal, para evitar derramamentos.
+        Ex:
+
+            int256 public peopleCount = 0;
+            mapping(uint => Person) public people; 
+
+            uint256 openingTime = EPOCH-TIME_DA_ABERTURA_AQUI
+
+            modifier onlyWhileOpen() {
+                require(block.timestamp >= openingTime)
+            }
+
+            struct Person {
+                uint _id;
+                string _firstName;
+                string _lastName;
+            }
+            
+            function addPerson(string memory _firstName, string memory _lastName) public onlyWhileOpen {
+            incrementCount();
+            people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+            }
+
+            function incrementCount() internal {
+                    peopleCount += 1;
+            }
+
+        Substituindo o código do owner por este, você terá algo semelhante a um "lançamento" do contrato, que só podera ser-
+        utilizado quando atingir o epoch-time
+        (lembre-se de não colocar um epoch-time muito longo, pois a imutabilidade não permite edições, logo terá que criar outro contrato)
+
+    */ 
 }
